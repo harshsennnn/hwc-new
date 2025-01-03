b856eb40234f74a6c590675952c444fd3606a540
HARSH <harshsennn@gmail.com>
2025-01-03 18:58:46 +0530

    badfile

--- src/main/java/com/iemr/hwc/bad.java
+++ src/main/java/com/iemr/hwc/bad.java
@@ -2,4 +2,4 @@ public class BadStyle{
     public static void main(String args[]){
 int x=10;
         String message="This is badly formatted";
-    System.out.println(message);}}
+System.out.println(message);}}
