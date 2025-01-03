4e4c245dbf135082d3e8da2cd61e3589eed16e02
HARSH <harshsennn@gmail.com>
2025-01-03 18:15:50 +0530

    bad

---
+++ src/main/java/com/iemr/hwc/bad.java
@@ -0,0 +1,6 @@
+public class BadCode {
+    public static void main(String[] args) {
+        int x=5; // no space around operator
+        System.out.println("hello world"); // no brackets around method call
+    }
+}
